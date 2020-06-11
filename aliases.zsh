b0fd86368672ffc0e012a53431272e7b40d36581
Charles Hendrie <chad.hendrie@merrillcorp.com>
2020-06-11 11:59:22 -0500

    Clean up projects

--- aliases.zsh
+++ aliases.zsh
@@ -19,38 +19,39 @@ alias lt='exa -T'
 alias ltd='exa -TD'
 alias h='fc -l 0'
 alias cat=bat
+alias vi=vim
 alias devd='cd ~/dev/projects/github.com/cehendrie/'
 
-alias vi=vim
-
-alias zshconfig='sublime ~/.zshrc'
-alias ohmyzsh='sublime ~/.oh-my-zsh'
-
+# git
 alias gpom='git push origin master'
 
-# local projects
-alias gitd='cd ~/dev/projects/github.devtools.merrillcorp.com/chendri/'
+# projects
 alias gitm='cd ~/dev/projects/github.com/cehendrie/'
+alias devb='cd ~/dev/repos/business/github.com/cehendrie/'
+alias devp='cd ~/dev/repos/personal/github.com/cehendrie/'
 alias red='cd ~/dev/projects/github.com/cehendrie/redaction-service'
+alias redh='cd ~/dev/projects/github.com/cehendrie/redaction-highlights-service'
 alias redp='cd ~/dev/projects/github.com/cehendrie/redaction-processing-service'
 alias redpl='cd ~/dev/projects/github.com/cehendrie/redaction-processing-service-large'
 alias reds='cd ~/dev/projects/github.com/cehendrie/redaction-search-service'
-alias redb='cd ~/dev/projects/github.com/cehendrie/redaction-batch-processing-service'
+alias redbp='cd ~/dev/projects/github.com/cehendrie/redaction-batch-processing-service'
 alias redc='cd ~/dev/projects/github.com/cehendrie/javelin-common-redaction'
 alias redpc='cd ~/dev/projects/github.com/cehendrie/javelin-common-redaction-processing'
-alias redt='cd ~/dev/projects/github.com/cehendrie/redaction-common-test'
+alias redct='cd ~/dev/projects/github.com/cehendrie/redaction-common-test'
 
-# deprecated datasite projects
+# deprecated projects
 alias redts='cd ~/dev/projects/github.com/cehendrie/redacted-terms-search-service'
 
-# Apple / OS X Specfic
-
+# apple / macOs
 alias sublime='open -a /Applications/Sublime\ Text.app/'
 alias textmate='open -a /Applications/TextMate.app/'
-
+alias bbedit='open -a /Applications/BBEdit.app/'
 # https://lucasfcosta.com/2019/02/10/terminal-guide-2019.html
 # https://github.com/lucasfcosta/dotfiles/blob/c01b73c6bd71b4888cae31440e14bf42297b3455/.aliases
-
 # Show/hide hidden files in Finder
 alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
 alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
+
+# zsh
+alias zshconfig='sublime ~/.zshrc'
+alias ohmyzsh='sublime ~/.oh-my-zsh'
