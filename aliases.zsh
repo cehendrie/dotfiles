ab60f2ab599316d1d56fc468643ac8ca18fdb057
Charles Hendrie <chad.hendrie@datasite.com>
2021-09-01 15:12:12 -0500

    Update zsh alias file

--- aliases.zsh
+++ aliases.zsh
@@ -13,45 +13,60 @@
 # https://the.exa.website
 
 alias ls='exa'
-alias ll='exa -la'
+alias ll='exa -lag'
 alias ll.='exa -ld .*'
 alias lt='exa -T'
 alias ltd='exa -TD'
+
 alias h='fc -l 0'
 alias cat=bat
 alias vi=vim
 alias gitm='cd ~/dev/projects/github.com/cehendrie/'
-alias devd='cd ~/dev/projects/github.com/cehendrie/'
-alias devb='cd ~/dev/repos/business/github.com/cehendrie/'
-alias devp='cd ~/dev/repos/personal/github.com/cehendrie/'
+alias devd='cd ~/dev/repos/github.com/cehendrie/'
 
 # git
 alias gpom='git push origin master'
+alias gpoma='git push origin main'
 
-# projects
-alias red='cd ~/dev/projects/github.com/cehendrie/redaction-service'
-alias redh='cd ~/dev/projects/github.com/cehendrie/redaction-highlights-service'
-alias redp='cd ~/dev/projects/github.com/cehendrie/redaction-processing-service'
-alias redpl='cd ~/dev/projects/github.com/cehendrie/redaction-processing-large-service'
-alias reds='cd ~/dev/projects/github.com/cehendrie/redaction-search-service'
-alias redbp='cd ~/dev/projects/github.com/cehendrie/redaction-batch-processing-service'
-alias redc='cd ~/dev/projects/github.com/cehendrie/redaction-common'
-alias redpc='cd ~/dev/projects/github.com/cehendrie/redaction-processing-common'
-alias redct='cd ~/dev/projects/github.com/cehendrie/redaction-common-test'
-alias reddr='cd ~/dev/repos/business/github.com/cehendrie/doc-reprocessing-service'
-alias redltt='cd ~/dev/repos/business/github.com/cehendrie/redaction-load-test-tool'
+# redaction projects
+alias red='cd ~/dev/repos/github.com/cehendrie/redaction-service'
+alias redh='cd ~/dev/repos/github.com/cehendrie/redaction-highlights-service'
+alias redi='cd ~/dev/repos/github.com/cehendrie/redaction-index-service'
+alias redil='cd ~/dev/repos/github.com/cehendrie/redaction-index-large-service'
+alias redr='cd ~/dev/repos/github.com/cehendrie/redaction-redact-service'
+alias reds='cd ~/dev/repos/github.com/cehendrie/redaction-search-service'
+alias redc='cd ~/dev/repos/github.com/cehendrie/redaction-common'
+alias redcp='cd ~/dev/repos/github.com/cehendrie/redaction-common-processing'
+alias redct='cd ~/dev/repos/github.com/cehendrie/redaction-common-test'
+alias redltt='cd ~/dev/repos/github.com/cehendrie/redaction-load-test-tool'
+alias reddr='cd ~/dev/repos/github.com/cehendrie/doc-reprocessing-service'
+alias redgm='cd ~/dev/repos/github.com/cehendrie/redaction-grid-mfe'
+alias rdr='cd ~/dev/repos/github.com/cehendrie/doc-reprocessing-service'
+alias drs='cd ~/dev/repos/github.com/cehendrie/doc-reprocessing-service'
 
-# deprecated projects
-alias redts='cd ~/dev/projects/github.com/cehendrie/redacted-terms-search-service'
+# smart tools projects
+alias sts='cd ~/dev/repos/github.com/cehendrie/smart-tools-service'
+alias ste='cd ~/dev/repos/github.com/cehendrie/smart-tools-events-service'
+alias stfl='cd ~/dev/repos/github.com/cehendrie/smart-tools-folder-labels-service'
+alias stt='cd ~/dev/repos/github.com/cehendrie/smart-tools-tracker-service'
+alias stc='cd ~/dev/repos/github.com/cehendrie/smart-tools-common'
+alias stct='cd ~/dev/repos/github.com/cehendrie/smart-tools-common-test'
+alias aicm='cd ~/dev/repos/github.com/cehendrie/ai-checklist-mapper-service'
+alias aisc='cd ~/dev/repos/github.com/cehendrie/ai-smart-categorization-service'
+alias aiscu='cd ~/dev/repos/github.com/cehendrie/ai-smart-categorization-utils'
+alias tli='cd ~/dev/repos/github.com/cehendrie/task-list-import-service'
+
+# datasite
+alias dsc='cd ~/dev/repos/github.com/cehendrie/ds1-configurations'
+alias dkm='cd ~/dev/repos/github.com/cehendrie/datasite-k8s-manifests'
 
 # apple / macOs
 alias sublime='open -a /Applications/Sublime\ Text.app/'
 alias textmate='open -a /Applications/TextMate.app/'
-alias bbedit='open -a /Applications/BBEdit.app/'
 
 # zsh
-alias zshconfig='sublime ~/.zshrc'
-alias ohmyzsh='sublime ~/.oh-my-zsh'
+alias zshconfig='textmate ~/.zshrc'
+alias ohmyzsh='textmate ~/.oh-my-zsh'
 
 function lsofport() {
 	lsof -n -i4TCP:$1 | grep LISTEN
